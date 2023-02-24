076d236bc889b456587bc6bcf23230e39014511a
kochan0802 <kochan19920802@gmail.com>
2023-02-25 09:38:02 +1000

    commit message

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -3,26 +3,49 @@ import axios from "axios";
 
 
 export const Pokemon = () => {
-  const [pokemons, setPokemons] = useState([]);
-    
+    const [pokemons, setPokemons] = useState([]);
+    const [search, setSearch] = useState('');
+    // const [japaneseNames, setJapanseNames] = useState({});
+
     useEffect(() => {
-            async function fetchData() {
+        async function fetchData() {
             const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
             setPokemons(response.data.results);
-            }
-    
-            fetchData();
-        },[]);
+        }
+        fetchData();
 
+    }, []);
+
+    const filteredPokemons = pokemons.filter(p => p.name.includes(search));
     
+    // useEffect(() => {
+    //     async function fetchJapaneseNames() {
+    //         const newJapaneseNames = {};
+    //         for (const p of pokemons) {
+    //             const response = await axios.get(p.url);
+    //             newJapaneseNames[p.name] = response.data.names.find(n => n.language.name === 'ja-Hrkt').name;
+    //         }
+    //         setJapanseNames(newJapaneseNames);
+    //     }
+    //     fetchJapaneseNames();
+    // }, [pokemons]);
+
+
+    // const filteredPokemon = pokemons.filter(p => japaneseNames[p.name].includes(search));
+        
     return (
         <div>
             <p>ポケモン言えるかな</p>
+            <input type="text" placeholder="ポケモン名で検索" onChange={e =>setSearch(e.target.value)} />
             <ul>
-                {pokemons.map(p => (
-                    <li key={p.name} >{p.name}</li>
+                {filteredPokemons.map(p => (
+                    <li key={p.name}>{p.name}</li>
                 ))}
+                {/* {filteredPokemon.map(p => (
+                    <li key={p.name} >{japaneseNames[p.name]}</li>
+                ))} */}
             </ul>
+            
         </div>
     );
 }
